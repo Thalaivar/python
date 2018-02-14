9d9816328dca115fe5be5d40cce6230545e32012
Thalaivar <dhruvlaad@outlook.com>
2018-02-14 13:21:59 +0530

    heirarchial smc on inverted pendulum on cart

--- inverted_pendulum_HSMC.py
+++ inverted_pendulum_HSMC.py
@@ -3,14 +3,6 @@ import matplotlib.pyplot as chart
 import math
 from scipy.integrate import ode
 
-# params_0 = Cc
-# params_1 = m
-# params_2 = M
-# params_3 = L
-# params_4 = Cp
-# params_5 = g
-
-
 ##### define signum function #####
 def sign(x):
     if x > 0:
@@ -24,56 +16,84 @@ def sign(x):
 
 
 def model(t, X, params):
-	
+	m, M, L, g, eta, l1, l2, gamma = params
+
 	x1, x2, x3, x4 = X
-	
-	Cc, m, M, L, Cp, g, l1, l2, gamma, eta = params
 
-	f1 = Cc*x2 - (Cp*x4 + m*g*L*math.sin(x3))*math.cos(x3)/L - m*L*(x4**2)*math.sin(x3)
+	f1 = m*L*(x4**2)*math.sin(x3) - m*g*math.sin(2*x3)/2
 	g1 = M + m*((math.sin(x3))**2)
-	f2 = Cc*x2 - m*L*(x4**2)*math.sin(x3) - Cp*x4*(m+M)/(m*L*math.cos(x3)) - (m*g*L*math.sin(x3)*(m+M))/(m*L*math.cos(x3))
-	g2 = -(m+M)*L*(M+m*((math.sin(x3))**2))
+	g2 = (M + m*((math.sin(x3))**2))/math.cos(x3)
+	d1 = (g*math.sin(x3))/L
 
-	s1 = x2 + l1*x1
-	s2 = x4 + l2*x3
+	s1 = x2 + l1*(x1 - 20)
+	s2 = x4 + l2*(x3 - math.pi)
 
 	S = s2 + gamma*s1
 
-	u_eq = ((f2/g2) - l2*x4 - gamma*l1*x2 + gamma*(f1/g1))/((1/g2) + (gamma/g1))
-	u_sw = -eta*S*np.tanh(S)/((1/g2) + (gamma/g1))
+	v_eq = -d1 - l2*x4 - gamma*l1*x2
+	v_sw = -eta*np.tanh(S)
 
-	u = u_eq + u_sw
+	v = (1/((gamma/g1) - 1/g2))*(v_eq + v_sw)
 
 	x1dot = x2
-	x2dot = (u - f1)/g1
+	x2dot = v/g1
 	x3dot = x4
-	x4dot = (u - f2)/g2
-
+	x4dot = d1 - v/g2
+	
 	return [x1dot, x2dot, x3dot, x4dot]
 
 def solve(params):
 	
 	t0 = 0
-	X0 = [0, 0, math.pi/2, 0]
-	t1 = 100
-	dt = 0.01
+	X0 = [0, 0, math.pi, 0]
+	t1 = 20
+	dt = 0.001
 	
-	r = ode(model).set_integrator('dopri5', nsteps = 1000, method='bdf').set_initial_value(X0, t0).set_f_params(params)
+	r = ode(model).set_integrator('dopri5', nsteps = 10000, method='bdf')
+	r.set_initial_value(X0, t0).set_f_params(params)
 
-	Y = np.zeros((10001,4))
-	t =[]
+	Y = np.zeros((t1/dt + 1, 5))
+	t =[0]
 	i = 0
 
 	while r.successful() and r.t < t1:
-		r.integrate(r.t + dt)
-		Y[i] = r.y
-		t.append(r.t)
 	
-	print Y
+		r.integrate(r.t + dt)
+		x1, x2, x3, x4 = r.y
+		m, M, L, g, eta, l1, l2, gamma = params
+		e_theta = x3*180.0/math.pi - 180
+		e_x = x1 - 20
+		
+		s1 = x2 + l1*(x1 - 20)
+		s2 = x4 + l2*(x3 - math.pi)
 
+		S = s2 + gamma*s1
+#		v_eq = -d1 - l2*x4 - gamma*l1*x2
+#		v_sw = -eta*np.tanh(S)
+#		v = (1/((gamma/g1) - 1/g2))*(v_eq + v_sw)
+ 		
+		Y[i] = [e_theta, e_x, s1, s2, S]
+		t.append(r.t)
+		i = i + 1
+	
+	print(np.size(Y, 0))
+	print(len(t))
+	chart.plot(t, Y[:,0], label='error_theta')
+	chart.plot(t, Y[:,1], label='error_x')
+	chart.legend(loc='upper right')
+	chart.grid()
+	chart.show()
+	
+	labels = ['s_x', 's_theta', 'S']
+	
+	for i in range(3):
+		chart.plot(t, Y[:,i+2], label=labels[i])
+	chart.legend(loc='upper right')	
+	chart.grid()
+	chart.show()
 ################################################################################
 if __name__ == '__main__':
-	
-	params = [0.09, 0.2, 1, 1, 0.05, 9.8, 6, 6, 7, 10]
-	
+
+	#params    m,   M, L,  g, eta, l1,  l2, gamma
+	params = [0.02, 2, 1, 9.8, 10, 0.01, 2, 30]	
 	solve(params)
